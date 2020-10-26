777063cdab840a8db530485942be9e9929ae419b
McCallWang <13567272790@163.com>
2020-10-26 10:35:33 +0800

    图片上传的接口基本完成

--- src/models/index.js
+++ src/models/index.js
@@ -8,33 +8,46 @@ AV.init({
 
 const Auth = {
   register(username, password) {
-    let user = new User();
-    user.setUsername(username);
-    user.setPassword(password);
+    let user = new User()
+    user.setUsername(username)
+    user.setPassword(password)
     return new Promise((resolve, reject) => {
       user.signUp().then(loginedUser => resolve(loginedUser), error => reject(error))
-    });
+    })
   },
 
   login(username, password) {
     return new Promise((resolve, reject) => {
-      User.logIn(username, password).then(loginedUser => resolve(loginedUser), error => reject(error));
-    });
+      User.logIn(username, password).then(loginedUser => resolve(loginedUser), error => reject(error))
+    })
   },
 
   logout() {
-    User.logOut();
+    User.logOut()
   },
 
   getCurrentUser() {
-    return User.current();
+    return User.current()
   }
 
 }
 
-
+const Uploader = {
+  add(file, filename) {
+    const item = new AV.Object('Image')
+    const avFile = new AV.File(filename, file)
+    item.set('filename', filename)
+    item.set('owner', AV.User.current())
+    item.set('url', avFile)
+    return new Promise((resolve, reject) => {
+      item.save()
+        .then((serverFile) => resolve(serverFile)).catch(error => reject(error))
+    })
+  }
+}
 
 
 export {
-  Auth
-};
+  Auth,
+  Uploader
+}
